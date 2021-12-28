ee4d18ae8e33c809727a5e7317f8269f6012e3a9
GES-mpalermo <mattia.palermo@greenenergystorage.eu>
2021-12-28 15:14:06 +0100

    Updated code version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name="GES-echem-suite",
-    version="0.1.3a",
+    version="0.1.4a",
     description="",
     long_description="",
     packages=["echemsuite"],
