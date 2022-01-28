9fe9a4e161c7ace58006c5bacdbf0b2052fe9050
GES-mpalermo <mattia.palermo@greenenergystorage.eu>
2022-01-28 13:08:06 +0100

    Fixed IS31 and incremented version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name="GES-echem-suite",
-    version="0.1.13a",
+    version="0.1.14a",
     description="",
     long_description="",
     packages=["echemsuite"],
